96aedefb4cae298b6295366663a512b14a861d4a
jennykim <jennykim97@gmail.com>
2021-12-20 22:12:10 +0900

    12강

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const express = require('express')
 const app = express()
 const port = 4000
 const bodyParser = require ('body-parser')   //bodyParser로 인해서 req.body를 받을 수 있게 됨
-const {User}= require('./models/User')
+const cookieParser = require('cookie-parser')
 const config = require('./config/key')
+
+const { User }= require('./models/User')
 
 // application/x-www-form-urlencoded 이렇게 된 데이터를 분석해서 가져올 수 있게 해줌
 app.use(bodyParser.urlencoded({extended:true}));
 // application/json 타입으로 된 것을 분석해서 가져올 수 있게 해줌
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI,{
@@ -43,9 +46,14 @@ app.post('/login', (req,res)=>{
     // password가 틀리다면
        if(!isMatch)   
            return res.json({loginSuccess:false, message:"비밀번호가 틀렸습니다"})
-     // password가 맞다면 토큰생성
-       userInfo.generateToken((err, userInfo) => { 
-    })
+     // password가 맞다면 토큰생성 (토큰 사용 위해 jsonwebtoken 라이브러리 다운로드)
+       userInfo.generateToken((err, userOk) => { 
+           if(err) return res.status(400).send(err)
+           // 토큰을 저장한다. 어디에? cookie, local storage ...
+           res.cookie("x_auth", userOk.token)     //userOk에 userInformation이 들어오기 때문에, userOk에는 token이 들어있다 (index.js의 generateToken부분 참고)
+           .status(200)
+           .json({loginSuccess:true, userId: userOk._id})  
+        })
 })
 })
 })
--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const saltRounds=10    //salt글자수
+const jwt= require('jsonwebtoken')
 
 const userSchema = mongoose.Schema({
     name:{
@@ -33,11 +34,13 @@ const userSchema = mongoose.Schema({
     }
 })
 
+// * 밑으로는, index.js에서 사용할 method 모음
+
 // index.js의 /register 에서 db에 save전에 실행됨 (pw암호화) / pre는 mongoose의 method
 userSchema.pre('save', function(next){    
     //비밀번호를 암호화 시킨다.
     //먼저 salt생성하고, 생성된 salt로 비밀번호를 암호화 시킨다.
-  var user = this;    //위에 userSchema를 뜻함
+  var user = this;    //위에 userSchema, 즉 db를 뜻함
 
   if(user.isModified('password')) {     //유저가 비번 항목 변경했을 때만 실행된다는 조건 (email 같은거 건드렸을시 실행되지 않음)
   bcrypt.genSalt(saltRounds, function(err,salt){
@@ -56,10 +59,24 @@ userSchema.pre('save', function(next){
 userSchema.methods.comparePassword = function(plainPassword, cb){
     // plainPassword = 사용자가 입력한 그대로 / 암호화된비번 = db에 담겨있다 -> plainPassword를 암호화하여 db의 비번과 비교해야함
     bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
-        if(err) return cb(err),    //다를때
+        if(err) return cb(err)    //다를때
         cb(null, isMatch)     //같을때: cb(err가 없고, isMatch가 true)
     })
 }
 
+userSchema.methods.generateToken = function(cb){
+    var user = this;
+    const token = jwt.sign(user._id.toHexString(), 'secretToken')     //_id는 db에 있는 항목이다
+    //user._id + 'secretToken' = token
+    //'secretToken'을 넣으면 user._id가 나오는 구조. 즉 이 사람이 누구인지 알 수 있음.
+    user.token = token   //생성한 token을 db의 token항목에 넣는다
+    user.save(function(err, userInformation) {    
+    //userInformation은 변수 ! 이 userInformation이 index.js로 가서 generateToken((err,userOk) => 의 userOk위치로 보내진다. 변수일 뿐. userInformation이 뭐지 userOk와 다른건가 궁금해할 필요 X (한 페이지의 한 함수 내의 변수들은 거기서 끝난다고 생각하기. 다른 페이지에서 불러와지면 그건 그 위치에 들어가는 변수일뿐, 이름 같다고 무조건 같나?하면 안됨)
+    // userInformation 변수 안에는 token을 담고 있다는게 중요
+       if(err) return cb(err)      //err가 있다면 콜백으로 err전달
+       cb(null, userInformation)   //save가 잘 되었다면 : 콜백으로 err가 없다는걸 전달하고, userInformation을 전달
+    })
+}
+
 const User= mongoose.model('User', userSchema)
-module.exports= {User}
+module.exports= { User }
