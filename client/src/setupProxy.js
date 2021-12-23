523c1f954e4e175b373d367d1a3b83d5746a8720
jennykim <jennykim97@gmail.com>
2021-12-23 16:59:13 +0900

    25강

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,4 +1,4 @@
-// CORS 설정하는 부분 (server port=4000, client port=3000 서로 포트가 다르므르 설정 필수)
+// proxy이용해 CORS 설정하는 부분 (server port=4000, client port=3000 서로 포트가 다르므르 설정 필수)
 
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
